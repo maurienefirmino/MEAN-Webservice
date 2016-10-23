8b56a03deb9b1db5c51da7ebbb6f6a7dec08876e
maurienefirmino <maurienefirmino@gmail.com>
2016-10-23 08:06:36 -0300

    conexão

--- db/config.js
+++ db/config.js
@@ -1,29 +1,34 @@
 'use strict';
 const mongoose = require('mongoose');
+const options = {
+	db: { native_parser: true }  
+	, server: { poolSize: 5 }
+	, replset: { rs_name: 'replica_set' }
+};
 
-const dbURI = 'mongodb://localhost/webservice';
+const dbURI = 'mongodb://localhost/webservice/?replicaSet=replica_set';
 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI,options);
 
 mongoose.connection.on('connected', function () {
-  console.log('Conexão aberta em: ' + dbURI);
+	console.log('Conexão aberta em: ' + dbURI);
 });
 
 mongoose.connection.on('error',function (err) {
-  console.log('Erro de Conexão: ' + err);
+	console.log('Erro de Conexão: ' + err);
 });
 
 mongoose.connection.on('disconnected', function () {
-  console.log('Conexão interrompida');
+	console.log('Conexão interrompida');
 });
 
 mongoose.connection.on('open', function () {
-  console.log('Conectado!');
+	console.log('Conectado!');
 });
 
 process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
-    console.log('Desconectado pelo webservice');
-    process.exit(0);
-  });
+	mongoose.connection.close(function () {
+		console.log('Desconectado pelo webservice');
+		process.exit(0);
+	});
 });
